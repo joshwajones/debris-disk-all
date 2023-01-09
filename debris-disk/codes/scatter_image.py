a8932c5f45d05a0e123f1ae39e13438cbedf6eed
Josh Jones <joshuajones@berkeley.edu>
2023-01-08 19:51:42 -0500

    revised (/1e5) thermal images

--- debris-disk/codes/scatter_image.py
+++ debris-disk/codes/scatter_image.py
@@ -101,7 +101,8 @@ def MakeImage_altonly(dustfile, d=10, maxa=100., aspect_ratio=1., resolution=0.0
             Tk2 = 500.7 * np.power(Rd, -0.4) * np.power(s, -0.2)
             Tk = np.where(check >= 1, Tk, Tk2)
 
-            intensity_alt = (L * consts.Lsun / 4. / np.pi) * Tk * emissivity * (s ** 2)
+            # intensity_alt = (L * consts.Lsun / 4. / np.pi) * Tk * emissivity * (s ** 2)
+            intensity_alt = Tk * emissivity * (s ** 2) / ( 1.0e5)
         elif not use_compHG:
             intensity_alt = (L * consts.Lsun / 4. / np.pi / (Rd * consts.au2cm) ** 2) * HG(-Yd_alt, g) * (
                         beta[i] * 10) ** -2

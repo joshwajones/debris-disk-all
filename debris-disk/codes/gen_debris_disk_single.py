5d62c063e632ebd150611fdc3c723fd083713238
Josh Jones <joshuajones@berkeley.edu>
2023-07-29 18:48:09 -0700

    cleaning things up

--- debris-disk/codes/gen_debris_disk_single.py
+++ debris-disk/codes/gen_debris_disk_single.py
@@ -1,208 +1,21 @@
-import numpy as np
 import debris_disk_single
-import consts
 import sys
 
 response = sys.argv[1]
 
 dd = debris_disk_single.DebrisDisk("../disk_input/%s.txt"%response)
+
 dd.AddSinglePlanet()
 dd.ComputeParentSingle(Nparticles=1, coll_in_middle=True)
 dd.ComputeParentOrbital()
 dd.ComputeDustGrains_Optimized()
 dd.OutputParentOrbit("../parentorbit/%s"%response)
 
-#
-#
-#
 dd.ComputeBackgroundParentSingle_Optimized()
-# #dd.ComputeBackgroundParentSingle()
 dd.ComputeBackgroundParentOrbital_Optimized()
-# #dd.ComputeBackgroundDustGrains()
-# #dd.ComputeBackgroundDustGrains_BetaOptimized2()
 dd.ComputeBackgroundDustGrains_Optimized()
-#
-# dd.ComputeForkParentSingle()
-# dd.ComputeForkParentOrbital()
-# #dd.ComputeForkDustGrains()
-# dd.ComputeForkDust_Optimized3(response)
 
-#dd.Plot_pqhk()
-#dd.Plot3DOrbit()
-
+dd.ComputeForkDust_Optimized3(response)
 
 dd.OutputDustAndBackOrbit("../dustorbit/%s"%response)
 
-
-# """
-# Generate debris disk
-# @author: Eve J. Lee
-# Feb. 16th 2016
-# """
-#
-# import numpy as np
-# import debris_disk_single
-# import consts
-# import sys
-#
-# #response = input("File name: ")
-# response = sys.argv[1]
-#
-# dd = debris_disk_single.DebrisDisk("../disk_input/%s.txt"%response)
-# dd.AddSinglePlanet()
-# #dd.ComputeParentSingle(manual=True, amin=62.6, amax=75.9, I0=0, e0=0.)
-# # dd.ComputeParentSingle(Nparticles=1, coll_in_middle=True)
-# # dd.ComputeParentOrbital()
-# # #dd.ComputeDustGrains()
-# # #dd.ComputeDustGrains_BetaOptimized()
-# # dd.ComputeDustGrains_Optimized()
-# # dd.OutputParentOrbit("../parentorbit/%s"%response)
-# # #dd.OutputQbeta("../images/imgrid/400x400/%s"%response)
-# # #dd.Plot3DOrbit()]
-# # #
-# # #
-# # #
-# # dd.ComputeBackgroundParentSingle_Optimized()
-# # #dd.ComputeBackgroundParentSingle()
-# # dd.ComputeBackgroundParentOrbital_Optimized()
-# # #dd.ComputeBackgroundDustGrains()
-# # #dd.ComputeBackgroundDustGrains_BetaOptimized2()
-# # dd.ComputeBackgroundDustGrains_Optimized()
-#
-# # dd.ComputeForkParentSingle()
-# # dd.ComputeForkParentOrbital()
-# # dd.ComputeForkDustGrains()
-# dd.ComputeForkDust_Optimized3(response)
-#
-# #dd.Plot_pqhk()
-# #dd.Plot3DOrbit()
-#
-#
-# dd.OutputDustAndBackOrbit("../dustorbit/%s"%response)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# # """
-# # Generate debris disk
-# #
-# # @author: Eve J. Lee
-# # Feb. 16th 2016
-# # """
-# #
-# # import numpy as np
-# # import debris_disk_single
-# # import consts
-# # import sys
-# #
-# # #response = input("File name: ")
-# # response = sys.argv[1]
-# #
-# # dd = debris_disk_single.DebrisDisk("../disk_input/%s.txt"%response)
-# # dd.AddSinglePlanet()
-# # #dd.ComputeParentSingle(manual=True, amin=62.6, amax=75.9, I0=0, e0=0.)
-# # dd.ComputeParentSingle(Nparticles=1)
-# # dd.ComputeParentOrbital()
-# # dd.ComputeDustGrains()
-# #
-# # #dd.Plot_pqhk()
-# # #dd.Plot3DOrbit()
-# #
-# # dd.OutputParentOrbit("../parentorbit/%s"%response)
-# # dd.OutputDustOrbit("../dustorbit/%s"%response)
-# #
-# # """
-# # Generate debris disk
-# # @author: Eve J. Lee
-# # Feb. 16th 2016
-# # """
-# #
-# # import numpy as np
-# # import debris_disk_single
-# # import consts
-# # import sys
-# #
-# # #response = input("File name: ")
-# # response = sys.argv[1]
-# #
-# # dd = debris_disk_single.DebrisDisk("../disk_input/%s.txt"%response)
-# # dd.AddSinglePlanet()
-# # #dd.ComputeParentSingle(manual=True, amin=62.6, amax=75.9, I0=0, e0=0.)
-# # dd.ComputeParentSingle(Nparticles=1, coll_in_middle=True)
-# # dd.ComputeParentOrbital()
-# # #dd.ComputeDustGrains()
-# # #dd.ComputeDustGrains_BetaOptimized()
-# # dd.ComputeDustGrains_Optimized()
-# # dd.OutputParentOrbit("../parentorbit/%s"%response)
-# # #dd.OutputQbeta("../images/imgrid/400x400/%s"%response)
-# # #dd.Plot3DOrbit()]
-# # #
-# # #
-# # #
-# # dd.ComputeBackgroundParentSingle_Optimized()
-# # #dd.ComputeBackgroundParentSingle()
-# # dd.ComputeBackgroundParentOrbital_Optimized()
-# # #dd.ComputeBackgroundDustGrains()
-# # #dd.ComputeBackgroundDustGrains_BetaOptimized2()
-# # dd.ComputeBackgroundDustGrains_Optimized()
-# #
-# # dd.ComputeForkParentSingle()
-# # dd.ComputeForkParentOrbital()
-# # #dd.ComputeForkDustGrains()
-# # dd.ComputeForkDust_Optimized3(response)
-# #
-# # #dd.Plot_pqhk()
-# # #dd.Plot3DOrbit()
-# #
-# #
-# # dd.OutputDustAndBackOrbit("../dustorbit/%s"%response)
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# # # """
-# # # Generate debris disk
-# # #
-# # # @author: Eve J. Lee
-# # # Feb. 16th 2016
-# # # """
-# # #
-# # # import numpy as np
-# # # import debris_disk_single
-# # # import consts
-# # # import sys
-# # #
-# # # #response = input("File name: ")
-# # # response = sys.argv[1]
-# # #
-# # # dd = debris_disk_single.DebrisDisk("../disk_input/%s.txt"%response)
-# # # dd.AddSinglePlanet()
-# # # #dd.ComputeParentSingle(manual=True, amin=62.6, amax=75.9, I0=0, e0=0.)
-# # # dd.ComputeParentSingle(Nparticles=1)
-# # # dd.ComputeParentOrbital()
-# # # dd.ComputeDustGrains()
-# # #
-# # # #dd.Plot_pqhk()
-# # # #dd.Plot3DOrbit()
-# # #
-# # # dd.OutputParentOrbit("../parentorbit/%s"%response)
-# # # dd.OutputDustOrbit("../dustorbit/%s"%response)
-# # #
